c554d74b593673d52351b8bdf0f76ced8ea121b4
Tatiana Chernishova <chernishovate@mail.ru>
2021-04-13 08:06:56 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -42,7 +42,7 @@ const loadPage = (url, dest = process.cwd(), config = {}) => {
   const pageName = genResourceName(`${pageLink.hostname}${pageLink.pathname === '/' ? '' : pageLink.pathname}`);
   const loadedPageName = `${pageName}.html`;
   const loadedResourcesDirname = `${pageName}_files`;
-  const loadedResourcesPath = path.join('tmp', loadedResourcesDirname);
+  const loadedResourcesPath = path.join('_fixtures_', loadedResourcesDirname);
   const loadedPagePath = path.join(dest, loadedPageName);
 
   const tasks = new Listr([]);
